09daffbd982c1e077a0cbb51425570ecdd5ea36e
nikita01021998 <nikitakumarin01021998@gmail.com>
2020-08-30 11:45:05 +0530

    Add chart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
-
+import './widgets/chart.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -30,6 +30,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
   ];
+
+  List<Transaction> get recentTransactionList{
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -74,14 +81,15 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(recentTransactionList),
+//            Container(
+//              width: double.infinity,
+//              child: Card(
+//                color: Colors.blue,
+//                child: Text('CHART!'),
+//                elevation: 5,
+//              ),
+//            ),
             TransactionList(_userTransactions),
           ],
         ),
